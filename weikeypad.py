fe87be9d4bb09134e1d339d7abc17c455694b76c
Robert Swirsky (thinkmate23) <robert@thrillscience.com>
2025-02-07 14:49:27 -0800

    Set pull-up/down values correctly. Clear ISR if no bits

--- weikeypad.py
+++ weikeypad.py
@@ -7,11 +7,12 @@ from rp2 import PIO
 # to handle arbitrary length Weigand messages
 @rp2.asm_pio(set_init=PIO.IN_HIGH)
 def rx_weigand():
-    set(x, 11)					# we will compare our pins to b'00011' if not equal, one pin is set
+    set(x, 3)					# we will compare our pins to b'00011' if not equal, one pin is set
     label('wait_for_data')
     in_(pins, 2)				#read two pins
     mov(y, isr)
     jmp(x_not_y, 'bit_set')		# if it's not b'00011', then one pin is set
+    mov(isr, 0)					# otherwise, clear it, and read some more
     jmp('wait_for_data')		# if not, keep looking
     label('bit_set')
     push()
@@ -20,7 +21,7 @@ def rx_weigand():
         
 # a bit is 40 microseconds wide, so we'll us a cycle rate of 10 microseconds so we
 # won't miss one
-pin0 = Pin(0, Pin.IN, Pin.PULL_DOWN)
+pin0 = Pin(0, Pin.IN, Pin.PULL_UP)
 sm = rp2.StateMachine(0, rx_weigand, freq=100000, in_base=pin0)
 sm.active(1)
 
@@ -31,6 +32,7 @@ while True:
     start_time = time.ticks_ms()
     elapsed_time = 0
     while True:
+        print(f"data = %d" % data)
         if data == 1:
             bits = bits + "1"
         elif data == 2:
@@ -40,7 +42,7 @@ while True:
         # Wait for either next bit or something on the fifo
         while (sm.rx_fifo() == 0):
             current_time = time.ticks_ms()
-            elapsed_time = time.ticks_diff(current_time, start_time))
+            elapsed_time = time.ticks_diff(current_time, start_time)
             if elapsed_time > 5:
                 break
         # check to see if we're here because of something in fifo or timeout
