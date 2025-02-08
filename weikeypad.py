cdd33a545d5cde347f407e62dca98d8f9a2e0f64
Robert Swirsky (laptop) <swirsky@gmail.com>
2025-02-07 16:41:13 -0800

    Trying different pullup/down options Switched to pin 16/17

--- weikeypad.py
+++ weikeypad.py
@@ -5,10 +5,11 @@ from rp2 import PIO
 
 # the PIO can only handle 32 bits, so we'll read one bit at a time here
 # to handle arbitrary length Weigand messages
-@rp2.asm_pio(set_init=PIO.IN_HIGH)
-def rx_weigand():
+@rp2.asm_pio()
+def rx_weigand():  # set_init=[PIO.IN_HIGH, PIO.IN_HIGH]
     set(x, 3)					# we will compare our pins to b'00011' if not equal, one pin is set
     label('wait_for_data')
+    mov(isr, 0)
     in_(pins, 2)				#read two pins
     mov(y, isr)
     jmp(x_not_y, 'bit_set')		# if it's not b'00011', then one pin is set
@@ -21,30 +22,36 @@ def rx_weigand():
         
 # a bit is 40 microseconds wide, so we'll us a cycle rate of 10 microseconds so we
 # won't miss one
-pin0 = Pin(0, Pin.IN, Pin.PULL_UP)
-sm = rp2.StateMachine(0, rx_weigand, freq=100000, in_base=pin0)
+pin16 = Pin(16, Pin.IN, Pin.PULL_DOWN)
+pin17 = Pin(17, Pin.IN, Pin.PULL_DOWN)
+sm = rp2.StateMachine(0, rx_weigand, freq=100000, in_base=pin16)
 sm.active(1)
 
 while True:
     bits = ""
     print("Waiting for a word")
     data = sm.get()				# wait for a bit detected from the FIFO
+    print("Starting read")
     start_time = time.ticks_ms()
     elapsed_time = 0
     while True:
-        print(f"data = %d" % data)
+        # print(f"data = %d" % data)
         if data == 1:
             bits = bits + "1"
         elif data == 2:
             bits = bits + "0"
         elif data == 0:
             pass
+        elif data == 3:
+            bits = bits + "?" # shouldn't ever happen
         else:
-            bita = bits + "?" # shouldn't ever happen
-        # Wait for either next bit or something on the fifo
-        data = 0
+            print(data)
+        
+        if len(bits) > 40:
+            break;
+        # Wait for either timeout or something on the fifo
+        current_time = time.ticks_ms()
         while (sm.rx_fifo() == 0):
-            current_time = time.ticks_ms()
             elapsed_time = time.ticks_diff(current_time, start_time)
             if elapsed_time > 5:
                 break
@@ -53,6 +60,7 @@ while True:
             break			# timeout. We're done
         if sm.rx_fifo() > 0:
             data = sm.get()
+            # print(data)
         else:
             data = 0
     # we've time out -- that means we're at the end of a word
