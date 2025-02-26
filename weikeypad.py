e9137247f66467754db5f65ecfc0f24fbc8a9621
Robert Swirsky (thinkmate23) <robert@thrillscience.com>
2025-02-26 14:10:59 -0800

    Transmit routine

--- weikeypad.py
+++ weikeypad.py
@@ -4,6 +4,15 @@ from bitstring import Bitarray
 import rp2
 from rp2 import PIO
 
+#37 bit weigand format
+# bits are 40 microseconds wide
+# bits are 2 ms apart
+
+# bit 1     - even parity over bits 2 through 18
+# bit 2-15  - Facility Code / Site ID
+# bit 16-36 - Card Id / Serial Number
+# bit 37    - Odd Parity over bits 19 through 36
+
 # the PIO can only handle 32 bits, so we'll read one bit at a time here
 # to handle arbitrary length Weigand messages
 @rp2.asm_pio()
@@ -25,56 +34,105 @@ def rx_weigand():  # set_init=[PIO.IN_HIGH, PIO.IN_HIGH]
     jmp(x_not_y, 'wait_for_zero')
     jmp('wait_for_data')
     
-        
-# a bit is 40 microseconds wide, so we'll us a cycle rate of 10 microseconds so we
-# won't miss one
-pin16 = Pin(16, Pin.IN, Pin.PULL_DOWN)
-pin17 = Pin(17, Pin.IN, Pin.PULL_DOWN)
-sm = rp2.StateMachine(0, rx_weigand, freq=2000000, in_base=pin16)
-sm.active(1)
-
-while True:
-    bits = ""
-    print("Waiting for a word")
-    data = sm.get()				# wait for a bit detected from the FIFO
-    start_time = time.ticks_ms()
-    elapsed_time = 0
-    while True:
-        if data == 1:
-            bits = bits + "1"
-        elif data == 2:
-            bits = bits + "0"
-        elif data == 0:
-            pass
-        elif data == 3:
-            print(data)
-            bits = bits + "?" # shouldn't ever happen
-        else:
-            print(data)
-        if len(bits) > 100:
-            break;
-        # Wait for either timeout or something on the fifo
-        current_time = time.ticks_ms()
-        elapsed_time = time.ticks_diff(current_time, start_time)
-        if sm.rx_fifo() > 0:
-            data = sm.get()
-        else:
-            data = 0
-        if elapsed_time > 100:
-            break
-    # we've time out -- that means we're at the end of a word
-    value = int(bits, 2)
-    print(f"Bits %s" % (bits))
-    print(f"Hex %s" % hex(value))
+@rp2.asm_pio()
+def tx_weigand():
+    pull()                  # 1
+    mov(x, osr)             # 1
+    jmp(not_x, "zero")      # 1
+    out(pins, 1)            # 1 set 1 pin high
+    nop()        [3]        # 3
+    out(pins, 0)            # 1 set pin low
+    jmp('wait2ms')          # 1 wait 2 milliseconds
+    label('zero')
+    out(pins, 2)            # 1 set zero pin high
+    nop()        [3]        # 3
+    label('wait2ms')        # 20 x 10 = 200 x 10 microseconds = 2 milliseconds
+    set(x, 19)              # 1
+    label("loop")
+    nop() [8]               # 8
+    jmp(x_dec, "loop")      # 1
     
-    # check parity if this is a 37-bit number
-    if len(bits) == 37:
-        a = bits[1:19].count('1')
-        parity_front = a % 2
-        b = bits[20:].count('1') + 1
-        parity_back = b % 2
-        print(f"Facility number = %s, Badge number = %s" % (a, b)
-        print(f"Parity bits should be %d and %d" % (parity_front, parity_back)
+    
+   
+class WeigandTranslator:
+    def __init__(self):
+        # a bit is 40 microseconds wide, so we'll us a cycle rate of 1/2 microsecond for receiving
+        # so we won't won't miss one. We may be able to slow this down
+        pin16 = Pin(16, Pin.IN, Pin.PULL_DOWN)
+        pin17 = Pin(17, Pin.IN, Pin.PULL_DOWN)
+        # start on PIO 0
+        self.sm = rp2.StateMachine(0, rx_weigand, freq=2000000, in_base=pin16)
+        self.sm.active(1)
+        
+        # for the transmit side, we will make the clock cycle rate 10 microseconds
+        # so 4 cycles = 1 bit width
+        pin14 = Pin(14, Pin.OUT)
+        pin15 = Pin(15, Pin.OUT)
+        # start on PIO 1
+        self.smx = rp2.StateMachine(4, tx_weigand, freq=100000, out_base=pin14)
+        self.smx.active(1)
+        
+        
+    
+    def Transmit(self, bits):
+        for b in bits:
+            # this blocks until space available, so no need to worry
+            # about overflow
+            self.smx.put(0 if b=='0' else 1)
+        # sleep a millisecond 
+        time.sleep(0.1)
+        
+        
+
+    def Receive(self):
+        bits = ""
+        print("Waiting for a word")
+        data = self.sm.get()				# wait for a bit detected from the FIFO
+        start_time = time.ticks_ms()
+        elapsed_time = 0
+        while True:
+            if data == 1:
+                bits = bits + "1"
+            elif data == 2:
+                bits = bits + "0"
+            elif data == 0:
+                pass
+            elif data == 3:
+                print(data)
+                bits = bits + "?" # shouldn't ever happen
+            else:
+                print(data)
+            if len(bits) > 100:
+                break;
+            # Wait for either timeout or something on the fifo
+            current_time = time.ticks_ms()
+            elapsed_time = time.ticks_diff(current_time, start_time)
+            if self.sm.rx_fifo() > 0:
+                data = self.sm.get()
+            else:
+                data = 0
+            if elapsed_time > 100:
+                break
+        # we've time out -- that means we're at the end of a word
+        value = int(bits, 2)
+        print(f"Bits %s" % (bits))
+        print(f"Hex %s" % hex(value))
+        
+        # check parity if this is a 37-bit number
+        if len(bits) == 37:
+            a = bits[1:18].count('1')
+            parity_front = a % 2
+            b = bits[18:36].count('1') + 1
+            parity_back = b % 2
+            print(f"Facility number = %s, Badge number = %s" % (a, b)
+            print(f"Parity bits should be %d and %d" % (parity_front, parity_back)
+        return bits
+       
+if __name__ == "__main__":
+    wt = WeigandTranslator()
+    while True:
+        bits = wt.Receive()
+        wt.Transmit(bits)
               
         
         
